091bc97630b7fcb2087c8ddfbb35d4293ef13dd2
Andrey Ivanov <Andreich2010@gmail.com>
2022-09-16 16:56:35 +0300

    Update clone_rows.js

--- clone_rows.js
+++ clone_rows.js
@@ -4,11 +4,19 @@ $('#add_more').click(function () {
 
 
 function cloneMore(selector, type) {
-    var newElement = $(selector).clone();
-    var total = $('#id_' + type + '-TOTAL_FORMS').val();
-
-
-    total++;
-    $('#id_' + type + '-TOTAL_FORMS').val(total);
-    $(selector).after(newElement);
+        var newElement = $(selector).clone(true);
+        var total = $('#id_' + type + '-TOTAL_FORMS').val();
+        newElement.find('input').each(function () {
+            var name = $(this).attr('name').replace('-' + (total - 1) + '-', '-' + total + '-');
+            var id = 'id_' + name;
+            $(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
+        });
+        newElement.find('select').each(function () {
+            var name = $(this).attr('name').replace('-' + (total - 1) + '-', '-' + total + '-');
+            var id = 'id_' + name;
+            $(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
+        });
+        total++;
+        $('#id_' + type + '-TOTAL_FORMS').val(total);
+        $(selector).after(newElement);
     }
